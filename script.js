c624a2236c6099d97b76aba3ca5fe55f8605fdb3
Martha D <45017656+mdempkey@users.noreply.github.com>
2019-11-13 22:08:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+var z = document.getElementById("sims1");
+var y = document.getElementById("sims2");
+var x = document.getElementById("sims3");
+var w = document.getElementById("sims4");
+
 function myFunction() {
-   var element = document.getElementById("myDIV");
-   element.classList.toggle("go");
+	var element = document.getElementById("sims1");
+	z.classList.toggle("go");
 }
 
 function myFunction1() {
-   var element = document.getElementById("myDIV1");
-   element.classList.toggle("go");
+	var element = document.getElementById("sims2");
+	y.classList.toggle("go");
+}
+
+function myFunction2() {
+	var element = document.getElementById("sims3");
+	x.classList.toggle("go");
+}
+
+function myFunction3() {
+	var element = document.getElementById("sims3");
+	w.classList.toggle("go");
 }
