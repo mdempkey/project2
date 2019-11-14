7a5953e0323cc3128d69776f98d9167beb00ffcf
Martha D <45017656+mdempkey@users.noreply.github.com>
2019-11-13 21:44:56 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("go");
+}
+
+function myFunction1() {
+   var element = document.getElementById("myDIV1");
+   element.classList.toggle("go");
+}
