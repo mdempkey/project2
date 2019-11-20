46684a16644f6196474a6a0d230b81c1853e73a6
Martha D <45017656+mdempkey@users.noreply.github.com>
2019-11-20 13:43:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ var i = document.getElementById("mor");
 var j = document.getElementById("goths1");
 var k = document.getElementById("goths2");
 var o = document.getElementById("myAudio"); 
+var z = document.getElementById("sims1");
+var y = document.getElementById("sims2");
+var x = document.getElementById("sims3");
+var w = document.getElementById("sims4");
 
 
 var image1array = [
@@ -141,3 +145,22 @@ function addImg3(){
 	o.play();
 }
 
+function myFunction() {
+	var element = document.getElementById("sims1");
+	z.classList.toggle("go");
+}
+
+function myFunction1() {
+	var element = document.getElementById("sims2");
+	y.classList.toggle("go");
+}
+
+function myFunction2() {
+	var element = document.getElementById("sims3");
+	x.classList.toggle("go");
+}
+
+function myFunction3() {
+	var element = document.getElementById("sims4");
+	w.classList.toggle("go");
+}
